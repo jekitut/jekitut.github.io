d868e56dba997a530ac66326ad8c25add64e1bdc
IRUL <44940036+jekitut@users.noreply.github.com>
2026-03-27 08:22:50 +0700

    Update Kabupaten.js

--- Daftar_CCTV/Indonesia/Jawa_Timur/Pasuruan/Kabupaten.js
+++ Daftar_CCTV/Indonesia/Jawa_Timur/Pasuruan/Kabupaten.js
@@ -1,12 +1,17 @@
 var Kabupaten_Pasuruan = [
-  { nama: "Purwodadi Arah Malang", lat: -7.8020282, lng: 112.7342435, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PWD_MLG.m3u8" },
-  { nama: "Purwodadi Arah Surabaya", lat: -7.8019527, lng: 112.7342936, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PWD_SBY.m3u8" },
-  { nama: "Exit Tol Purwodadi", lat: -7.8009709, lng: 112.7349112, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/EXIT_TOL_PURWODADI.m3u8" },
-  { nama: "Purwosari Arah Malang", lat: -7.7714673, lng: 112.7462287, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PURWOSARI_MLG.m3u8" },
-  { nama: "Purwosari Arah Utara", lat: -7.7718087, lng: 112.7459488, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PURWOSARI_SBY.m3u8" },
-  { nama: "Taman Dayu Arah Selatan", lat: -7.6652479, lng: 112.699705, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/TAMANDAYU.m3u8" },
-  { nama: "Taman Dayu Arah Utara", lat: -7.6651849, lng: 112.6996343, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/TAMANDAYUUTARA.m3u8" },
-  { nama: "Pandaan Arah Timur", lat: -7.6542526, lng: 112.6953054, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/P21_KARTINI.m3u8" },
-  { nama: "Pandaan Arah Bangil", lat: -7.6543632, lng: 112.6954089, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/P21_BANGIL.m3u8" },
-  { nama: "Patung Sapi", lat: -7.6441809, lng: 112.6896415, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PATUNGSAPI.m3u8" }
+  { nama: "Purwodadi Arah Malang", lat: -7.802030, lng: 112.734200, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PWD_MLG.m3u8" },
+  { nama: "Purwodadi Arah Surabaya", lat: -7.801950, lng: 112.734250, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PWD_SBY.m3u8" },
+  { nama: "Exit Tol Purwodadi", lat: -7.800750, lng: 112.734925, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/EXIT_TOL_PURWODADI.m3u8" },
+  { nama: "Purwosari Arah Selatan", lat: -7.771525, lng: 112.746175, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PURWOSARI_MLG.m3u8" },
+  { nama: "Purwosari Arah Utara", lat: -7.771800, lng: 112.746000, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PURWOSARI_SBY.m3u8" },
+  { nama: "Taman Dayu Arah Selatan", lat: -7.665175, lng: 112.699650, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/TAMANDAYU.m3u8" },
+  { nama: "Taman Dayu Arah Utara", lat: -7.665150, lng: 112.699625, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/TAMANDAYUUTARA.m3u8" },
+  { nama: "Pandaan Arah Selatan", lat: -7.654275, lng: 112.695375, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/P21_KARTINI.m3u8" },
+  { nama: "Pandaan Arah Timur", lat: -7.654325, lng: 112.695425, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/P21_BANGIL.m3u8" },
+  { nama: "Polsek Pandaan Arah Malang", lat: -7.653950, lng: 112.687675, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/POLSEKPDN_1.m3u8" },
+  { nama: "Polsek Pandaan Arah Surabaya", lat: -7.654150, lng: 112.687775, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/POLSEKPDN_2.m3u8" },
+  { nama: "Patung Sapi", lat: -7.644200, lng: 112.689625, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/PATUNGSAPI.m3u8" },
+  { nama: "Apollo Arah Malang", lat: -7.580625, lng: 112.691175, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/APOLLO_GEMPOL.m3u8" },
+  { nama: "Gempol Arah Selatan", lat: -7.547625, lng: 112.697325, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/SP3_GEMPOL.m3u8" },
+  { nama: "Gempol Arah Utara", lat: -7.547825, lng: 112.697425, url: "https://dishub.pasuruankab.go.id:5444/LiveApp/streams/SP3_GEMPOL_PORONG.m3u8" }
 ];
