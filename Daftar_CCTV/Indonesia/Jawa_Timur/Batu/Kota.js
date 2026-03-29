69c94bb2df2365b8c350e124595f36db45aa01ee
IRUL <44940036+jekitut@users.noreply.github.com>
2026-03-30 06:45:35 +0700

    Add files via upload

--- Daftar_CCTV/Indonesia/Jawa_Timur/Batu/Kota.js
+++ Daftar_CCTV/Indonesia/Jawa_Timur/Batu/Kota.js
@@ -2,6 +2,11 @@ var Kota_Batu = [
   { kode: "CCTV (Dishub) Pintu Masuk Dau Batu", nama: "Gerbang Kota Wisata Batu", lat: -7.910500, lng: 112.580600, url: "https://stream.batukota.go.id/WebRTCApp/streams/03rZ8eUb0IAb8Fn41735010648573.m3u8" },
   { kode: "CCTV (Kominfo) Depan Rumah Dinas Walikota Batu", nama: "Rumah Dinas Walikota", lat: -7.867875, lng: 112.516600, url: "https://stream.batukota.go.id/WebRTCApp/streams/csC0gk1uL2aCzbWM1710472601306.m3u8" },
   { kode: "CCTV Kominfo Depan Pertigaan sumbergondo", nama: "Punten", lat: -7.835300, lng: 112.528700, url: "https://stream.batukota.go.id/WebRTCApp/streams/JvvohCsOIgnpsxMQ1707188028701.m3u8" },
-  
-  { kode: "CCTV Taman Hutan Kota Bondas Jl. Sultan Agung", nama: "Taman Hutan Kota Batu", lat: -7.878775, lng: 112.525075, url: "https://stream.batukota.go.id/WebRTCApp/streams/c4umuPuVX9rYo8pJ1705741554912.m3u8" }
+
+
+  { kode: "CCTV Alun-Alun Sisi Barat Kota Batu", nama: "Alun-alun Sisi Barat", lat: -7.871150, lng: 112.526375, url: "https://stream.batukota.go.id/WebRTCApp/streams/Nun07TbGCmzdFWcO1705636022700.m3u8" },
+  { kode: "CCTV Alun-Alun Sisi Tengah Kota Batu", nama: "Alun-alun Sisi Tengah", lat: -7.871150, lng: 112.526925, url: "https://stream.batukota.go.id/WebRTCApp/streams/EiSJaogqiTYQ8ONQ1705576581602.m3u8" },
+  { kode: "CCTV Alun-Alun Sisi Timur Kota Batu", nama: "Alun-alun Sisi Timur", lat: -7.871425, lng: 112.527450, url: "https://stream.batukota.go.id/WebRTCApp/streams/EiSJaogqiTYQ8ONQ1705576581602.m3u8" },
+  { kode: "CCTV Taman Hutan Kota Bondas Jl. Sultan Agung", nama: "Taman Hutan Kota Batu", lat: -7.878775, lng: 112.525075, url: "https://stream.batukota.go.id/WebRTCApp/streams/c4umuPuVX9rYo8pJ1705741554912.m3u8" },
+  { kode: "CCTV Jl. Dewi Sartika Pasar Induk Kota Batu", nama: "Pasar Induk Among Tani", lat: -7.882450, lng: 112.533500, url: "https://stream.batukota.go.id/WebRTCApp/streams/Ma9A5BZjf1eMJde61705741594487.m3u8" }
 ];
