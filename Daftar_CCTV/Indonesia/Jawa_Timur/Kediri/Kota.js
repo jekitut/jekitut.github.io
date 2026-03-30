5b301f052c689b418ba382d676e2553914a50121
IRUL <44940036+jekitut@users.noreply.github.com>
2026-03-30 20:19:33 +0700

    Add files via upload

--- Daftar_CCTV/Indonesia/Jawa_Timur/Kediri/Kota.js
+++ Daftar_CCTV/Indonesia/Jawa_Timur/Kediri/Kota.js
@@ -3,6 +3,7 @@ var Kota_Kediri = [
   { kode: "2", nama: "Bandar Ngalim", lat: -7.827125, lng: 112.0057075, url: "https://pplterpadu.kedirikota.go.id:8888/bandar_ngalim/stream.m3u8" },
   { kode: "3", nama: "Alun-alun", lat: -7.827700, lng: 112.010500, url: "https://pplterpadu.kedirikota.go.id:8888/alun_alun/stream.m3u8" },
   { kode: "4", nama: "Tamanan", lat: -7.828250, lng: 111.984975, url: "https://pplterpadu.kedirikota.go.id:8888/tamanan/stream.m3u8" },
+  { kode: "7", nama: "Semampir", lat: -7.793775, lng: 112.009800, url: "https://pplterpadu.kedirikota.go.id:8888/semampir/stream.m3u8" },
   { kode: "8", nama: "Mrican", lat: -7.780675, lng: 112.000550, url: "https://pplterpadu.kedirikota.go.id:8888/mrican/stream.m3u8" },
   { kode: "9", nama: "Iskandar Muda", lat: -7.795150, lng: 112.001850, url: "https://pplterpadu.kedirikota.go.id:8888/iskandar_muda/stream.m3u8" },
   { kode: "10", nama: "Jetis", lat: -7.837475, lng: 112.009100, url: "https://pplterpadu.kedirikota.go.id:8888/jetis/stream.m3u8" },
