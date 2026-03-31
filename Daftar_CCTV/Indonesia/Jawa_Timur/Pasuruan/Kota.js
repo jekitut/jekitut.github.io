8d3fea32dbba138493a7315e41c587c666a84d93
IRUL <44940036+jekitut@users.noreply.github.com>
2026-04-01 06:27:58 +0700

    Add files via upload

--- Daftar_CCTV/Indonesia/Jawa_Timur/Pasuruan/Kota.js
+++ Daftar_CCTV/Indonesia/Jawa_Timur/Pasuruan/Kota.js
@@ -1,5 +1,6 @@
 var Kota_Pasuruan = [
-  { kode: "Batas Selatan Kota 2", nama: "Tembokrejo", lat: -7.679200, lng: 112.899200, url_path: "bts-tembokrejo" }
+  { kode: "167 - Batas Selatan Kota 2 Arah Selatan", nama: "Tembokrejo Arah Selatan", lat: -7.679200, lng: 112.899200, url_path: "bts-tembokrejo" },
+  { kode: "169 - Simpang Kebonagung Arah Selatan", nama: "Kebonagung Arah Selatan", lat: -7.662500, lng: 112.893150, url_path: "sm-kebonagung-2" }
 ];
 
 Kota_Pasuruan.forEach(item => {
